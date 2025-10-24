87e97acd35bb584bf0c6456ea293d3dfbff65d55
Saumya Nayak <saumyo224@gmail.com>
2025-10-24 19:12:25 +0530

    imported own custom fonts and used it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "QuickSand-Light": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "QuickSand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
