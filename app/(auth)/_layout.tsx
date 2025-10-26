37db47c8da6c4f7ed15842f32d5eb250370a7c69
Saumya Nayak <saumyo224@gmail.com>
2025-10-26 16:22:42 +0530

    auth layout completed

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,13 +1,43 @@
-import { View, Text } from "react-native";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 import { Slot } from "expo-router";
+import React from "react";
+import {
+  Dimensions,
+  Image,
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 const _layout = () => {
   return (
-    <SafeAreaView>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      className="h-full"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View
+          className="w-full relative"
+          style={{ height: Dimensions.get("screen").height / 2.25 }}
+        >
+          <ImageBackground
+            source={images.loginGraphic}
+            className="size-full rounded-b-lg"
+            resizeMode="stretch"
+          />
+          <Image
+            source={images.logo}
+            className="size-48 self-center absolute -bottom-16 z-10"
+          />
+        </View>
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
