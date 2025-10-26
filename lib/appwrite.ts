e427710379269ce512c58c049215095a0f985e91
Saumya Nayak <saumyo224@gmail.com>
2025-10-26 13:18:23 +0530

    updated appwrite config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,7 @@
 export const appwriteConfig = {
     endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
     projectID : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
-    platform : "com.saumya.fastfood"
+    platform : "com.saumya.fastfood",
+    databaseID : process.env.EXPO_PUBLIC_APPWRITE_DB_ID,
+    userTableID : process.env.EXPO_PUBLIC_APPWRITE_USER_TABLE
 }
