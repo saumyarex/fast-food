ebaaf2c31fe952f1b5abcb57e34575093f4968d5
Saumya Nayak <saumyo224@gmail.com>
2025-10-26 16:23:52 +0530

    SignIn UI done

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,58 @@
-import { View, Text } from "react-native";
-import React from "react";
+import CustomButton from "@/components/CustomButton";
+import CustomInput from "@/components/CustomInput";
+import { Link, router } from "expo-router";
+import React, { useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 const SignIn = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      return Alert.alert("Error", "Please provide valid email and password");
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      Alert.alert("Success", "Signed in successfully");
+      router.replace("/");
+    } catch (error: any) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
+
   return (
-    <View>
-      <Text>SignIn</Text>
+    <View className="gap-10 bg-white rounded-lg p-5 mt-5">
+      <CustomInput
+        label="Email"
+        value={form.email}
+        placeholder="Enter your email"
+        keyboardType="email-address"
+        onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+      />
+      <CustomInput
+        label="Password"
+        placeholder="Enter your password"
+        value={form.password}
+        secureTextEntry={true}
+        onChangeText={(text) =>
+          setForm((prev) => ({ ...prev, password: text }))
+        }
+      />
+      <CustomButton title="Sign In" onPress={submit} isLoading={isSubmitting} />
+
+      <View className="flex justify-center mt-0 flex-row gap-2">
+        <Text className="base-regular text-gray-100">
+          Don&apos;t have an account?
+        </Text>
+        <Link href={"/sign-up"} className="base-bold text-primary">
+          Sign Up
+        </Link>
+      </View>
     </View>
   );
 };
