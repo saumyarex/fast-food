a5df8ec3d83ff2e462c846d1cd94017966a35ced
Saumya Nayak <saumyo224@gmail.com>
2025-10-26 12:44:53 +0530

    imported appwrite config details

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint : process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectID : process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform : "com.saumya.fastfood"
+}
