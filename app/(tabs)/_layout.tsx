803709044ba64de9fe6c7a5c18d75b51c94efc00
Saumya Nayak <saumyo224@gmail.com>
2025-10-28 23:44:38 +0530

    added function to fetch current user while loading the app

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,12 @@
+import useAuthStore from "@/store/auth.store";
 import { Redirect, Slot } from "expo-router";
 import React from "react";
 
-const _layout = () => {
-  const isAuthenticated = false;
+const TabLayout = () => {
+  const { isAuthenticated } = useAuthStore();
 
   if (!isAuthenticated) return <Redirect href={"/sign-in"} />;
   return <Slot />;
 };
 
-export default _layout;
+export default TabLayout;
